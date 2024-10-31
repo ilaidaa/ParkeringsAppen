c6ad1bf2440bd834aa8749e02c2ebf5178ca2c59
ilaidaa <ilaida.e@hotmail.com>
2024-10-31 12:10:35 +0100

    I klassen Vehicle (basklassen) har det skapats en metod för att generera slumpmässiga egenskaper till fordon.

--- ParkeringsAppen/Vehicles.cs
+++ ParkeringsAppen/Vehicles.cs
@@ -6,30 +6,62 @@ using System.Threading.Tasks;
 
 namespace ParkeringsAppen
 {
-  
-        public class Vehicles
+
+    public class Vehicles
+    {
+
+        //Basklassen fordon
+        public abstract class Vehicle
         {
+            //Skapa propertys
+            public string Colour { get; set; }
+            public string LPlate { get; set; }
 
-            //Basklassen fordon
-            public abstract class Vehicle
+
+            //Construktor
+            public Vehicle(string colour, string lPlate)
             {
-                //Skapa propertys
-                public string Colour { get; set; }
-                public string LPlate { get; set; }
+                Colour = colour;
+                LPlate = lPlate;
+            }
 
 
-                //Construktor
-                public Vehicle(string colour, string lPlate)
+
+            //En metod i basklassen som är tom, vilket innebär att dne måste fyllas i subklasserna.
+            //Detta skapar möjighet för subklasserna att göra metoden på sitt egna sätt som skiljer från resterande klasser
+            //Just denna metod är tänkt att ge information om fordonen
+            public abstract void DisplayInfo();
+
+
+
+
+
+            //Metod för att generera ett slumpmässigt fordon
+            //Basklassens första metod
+            public Vehicle GenerateRandomVehicle()
+            {
+                string[] colors = { "Röd", "Svart", "Grön", "Gul", "Blå" };
+                string color = colors[Random.Shared.Next(colors.Length)];
+                string licensePlate = $"ABC{Random.Shared.Next(100, 999)}"; // Enkelt registreringsnummer
+
+                int vehicleType = Random.Shared.Next(1, 4); // 1 = Car, 2 = MC, 3 = Bus
+                switch (vehicleType)
                 {
-                    Colour = colour;
-                    LPlate = lPlate;
+                    case 1:
+                        bool isElectric = Random.Shared.Next(2) == 1;
+                        return new Car(color, licensePlate, isElectric);
+                    case 2:
+                        string[] brands = { "Harley", "Yamaha", "Kawasaki" };
+                        string brand = brands[Random.Shared.Next(brands.Length)];
+                        return new MC(color, licensePlate, brand);
+                    case 3:
+                        int passengers = Random.Shared.Next(10, 60);
+                        return new Bus(color, licensePlate, passengers);
+                    default:
+                        return null;
                 }
 
 
-                //En metod i basklassen som är tom, vilket innebär att dne måste fyllas i subklasserna.
-                //Detta skapar möjighet för subklasserna att göra metoden på sitt egna sätt som skiljer från resterande klasser
-                //Just denna metod är tänkt att ge information om fordonen
-                public abstract void DisplayInfo();
 
             }
 
@@ -120,4 +152,4 @@ namespace ParkeringsAppen
         }
 
     }
-
+}
