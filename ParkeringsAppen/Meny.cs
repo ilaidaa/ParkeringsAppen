77e7db5c282dfb5496778a7496b59fc10f9168fc
z4kdj <zakaria.djelassi@outlook.com>
2024-11-05 12:43:37 +0100

    Lade till en metod som heter CollectVehicleInfo som samlar in mer information av användaren om fordonet.
    Uppdaterat menyvalet med TryParse. Inte helt klar än.

--- ParkeringsAppen/Meny.cs
+++ ParkeringsAppen/Meny.cs
@@ -1,80 +1,112 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ParkeringsAppen.Vehicles.Vehicle;
+using static ParkeringsAppen.Vehicles;
 
 namespace ParkeringsAppen
 {
-        internal class Meny
+    // Intern klass Meny ansvarar för att visa en meny och hantera användarens val för parkering
+    internal class Meny
+    {
+        // Metod för att visa menyn och hantera användarens val
+        public void ShowMeny()
         {
-            //En metod som ska hantera val när ett fordon anlädder till parkeringen
-            public void ShowMenu()
+            
+            // Oändlig loop för att hålla menyn uppe tills användaren avslutar
+            while (true)
             {
-                Console.WriteLine("Hej! Välkommen till ParkeringSöder!");
-                Console.WriteLine(); //För design
-                Console.WriteLine("1. Parkera BIL");
-                Console.WriteLine("2. Parkera MC");
-                Console.WriteLine("3. Parkera BUSS");
-                Console.WriteLine("4. Jag är Parkeringsvakt");
-                Console.WriteLine("5. Jag är Parekringschef");
-                Console.WriteLine(); //För design
-
-                //Variabel för att lagra användaren val den ska in i switch satsen längre ner sen
+                // Rensar konsolen för att visa menyn
+                Console.Clear();
+                Console.WriteLine("Välkommen till ParkeringSöder!");
+                Console.WriteLine("1.Parkera ett slumpmässigt fordon");
+                Console.WriteLine("2.Visa alla parkerade fordon");
+                Console.WriteLine("3.Parkeringsvakt - Kontrollera överträdande fordon");
+                Console.WriteLine("4.Parkeringschef - Visa statistik");
+                Console.WriteLine("5.Checka ut fordon");
+                Console.WriteLine("6.Avsluta");
+                
                 int choice;
-
-
-                //en evighets loop som ska köra till användaen skriver in rätt tal. Syftet med loopen är att hantera fel inmatningar av användaren
-                while (true)
+                // Försöker läsa användarens val och konvertera det till ett heltal
+                if (int.TryParse(Console.ReadLine(), out choice))
                 {
-                    Console.Write("Vänligen knappa in heltalet ovan som passar in dig bäst: ");
-
-                    // Försök att parsa ANVÄNDARENS input till ett heltal, choice deklarerade du innan whileloppen
-                    if (int.TryParse(Console.ReadLine(), out choice))
+                    Console.Clear();
+                    // Switch-sats för att hantera användarens val
+                    switch (choice)
                     {
-                        // Om input är inom giltiga alternaticv vilket är från 1-5, bryt ut ur loopen
-                        if (choice >= 1 && choice <= 5)
-                        {
+                        case 1:
+                            //Gör en slumpgenererator som skapar en slumppad fordon
+                           
+                            //Lägg in metoden CollectVehicleInfo som tar en slumpad  bil.
+                           
                             break;
-                        }
-                        else //Om den inte är det be användare försöka igen
-                        {
-                            Console.Write("Vänligen ange ett tal mellan 1 och 5: ");
-                        }
+                        case 2:
+                           
+                            break;
+                        case 3:
+                           
+                            break;
+                        case 4:
+                            
+                            break;
+                        case 5:
+                            
+                            break;
+                        case 6:
+                            
+                            return;
+                        default:
+                            // Hanterar ogiltiga val (siffror som inte motsvarar ett menyval)
+                            Console.WriteLine("Ogiltigt val. Försök igen.");
+                            break;
                     }
-                    else
-                    {
-                        Console.WriteLine("Ogiltig inmatning. Vänligen ange ett heltal.");
-                    }
+                    // Väntar på att användaren ska trycka på en tangent för att återgå till menyn
+                    Console.WriteLine("\nTryck på valfri tangent för att återgå till menyn...");
+                    Console.ReadKey();
                 }
-
-                // Hantera val baserat på användarens input
-                switch (choice)
+                else
                 {
-                    case 1:
-                        // Kod för att hantera parkering av bil Kommer senare
-                        break;
-                    case 2:
-                        // Kod för att hantera parkering av MC Kommer senare
-                        break;
-                    case 3:
-                        // Kod för att hantera parkering av buss Kommer senare
-                        break;
-                    case 4:
-                        // Kod för parkeringsvaktens alternativ Kommer senare
-                        break;
-                    case 5:
-                        // Kod för parkeringschefens alternativ Kommer senare
-                        break;
-                    default:
-                        Console.WriteLine("Ogiltigt val. Vänligen försök igen.");
-                        break;
+                    // Meddelande för ogiltig inmatning om användaren inte anger ett heltal
+                    Console.WriteLine("Ogiltig inmatning. Ange ett heltal.");
                 }
-
             }
+        }
 
 
 
+
+        // Privat metod för att samla in ytterligare information om det genererade fordonet
+        private void CollectVehicleInfo(Vehicle vehicle)
+        {
+            // Om fordonet är en bil, fråga om bilen är elektrisk
+            if (vehicle is Car car)
+            {
+                Console.Write("Är bilen elektrisk ? (ja / nej) : ");
+                car.Electric = Console.ReadLine()?.ToLower() == "ja";
+            }
+            // Om fordonet är en motorcykel, fråga efter motorcykelns märke
+            else if (vehicle is MC mc)
+            {
+                Console.Write("Ange motorcykelns märke: ");
+                mc.Brand = Console.ReadLine();
+            }
+            // Om fordonet är en buss, fråga efter antalet passagerare
+            else if (vehicle is Bus bus)
+            {
+                Console.Write("Ange antalet passagerare: ");
+                if (int.TryParse(Console.ReadLine(), out int passengers))
+                {
+                    bus.Passengers = passengers;
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt antal passagerare.");
+                }
+            }
         }
     }
+}
 
