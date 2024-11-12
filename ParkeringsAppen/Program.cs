1d308fb966a3ab9b58f5272be4e3b29a7bdf543b
Rabar Mohamed <rabar003@hotmail.com>
2024-11-12 12:28:46 +0100

    Anropa allt till Main programmet.

--- ParkeringsAppen/Program.cs
+++ ParkeringsAppen/Program.cs
@@ -3,8 +3,18 @@
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hejsan världen!");
+
+            // Skapa en instans av Meny och visa huvudmenyn
+
+            Meny meny = new Meny();
+
+            meny.ShowMeny();
+
+
+            // Programmet körs tills användaren väljer att avsluta i Meny-klassen
+
         }
     }
 }
